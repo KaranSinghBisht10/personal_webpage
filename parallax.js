5d7c3456b86b8a9439edf2bd6a86e6b0fc9dad19
Karan Singh Bisht <Bishtkaransingh10@gmail.com>
2023-07-18 22:22:22 +0530

    commited on 18

--- parallax.js
+++ parallax.js
@@ -3,13 +3,17 @@ let leaf= document.getElementById("leaf");
 let hill1=document.getElementById("hill1");
 
 let scroll=window.addEventListener('scroll',()=>{
-  let value=window.scrollY;
+  let value = Math.min(window.scrollY, 1000);
 
   text.style.marginTop=value*2.5+'px';
   
   leaf.style.top=value*-1.5+'px';
   leaf.style.left=value*+1.5+'px';
   hill1.style.top=value*-0.3+'px';
-
+  let opacity = 1 - value / 1000; // Adjust the divisor to control the fading range
+  text.style.opacity = opacity;
 
 })
+
+
+let art=document.getElementsByClassName(art);
